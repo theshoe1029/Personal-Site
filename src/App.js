8deca0b6b63677141882a19853f30c19379d75c5
adamschueller <aschueller@zoominternet.net>
2020-04-19 20:41:28 -0400

    added green

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-var colorLight = '#cc6bff'
-var colorDark = '#b523ff'
-
 class App extends React.Component {
   constructor(props){
     super(props)
@@ -20,19 +17,13 @@ class App extends React.Component {
   }
 
   render() {
-
-    var navStyle = {
-      backgroundColor: colorLight,
-      borderBottom: '0.25rem solid ' + colorDark
-    }
-
-    var linkStyle = {color: colorLight}
-
     return (
       <div>
         <a id="resume-link" href="https://drive.google.com/file/d/1Yc14-pB_1-PVFkxMnM71S74sO6Z2bGUr/view?usp=sharing" target="_">
-          <nav className="navbar" id="navigation" style={navStyle}>
-            <h2 className="mx-auto text-center">Adam Schueller</h2>
+          <nav className="navbar" id="navigation">
+            <h2 className="mx-auto text-center">
+              <img src="images/header.gif"></img>
+            </h2>
           </nav>
         </a>
 
@@ -40,12 +31,12 @@ class App extends React.Component {
 
         </div>
 
-        <footer className="bg-light" id="footer">
+        <footer id="footer">
           <div id="footer-div">
-            <a href="https://drive.google.com/file/d/1Yc14-pB_1-PVFkxMnM71S74sO6Z2bGUr/view?usp=sharing" style={linkStyle}><i className="fas fa-file-alt align-middle"></i></a>
-            <a href="mailto:as174@rice.edu" style={linkStyle}><i className="far fa-envelope align-middle"></i></a>
-            <a href="https://www.linkedin.com/in/adam-schueller/" style={linkStyle}><i className="fab fa-linkedin-in align-middle"></i></a>
-            <a href="https://github.com/theshoe1029" style={linkStyle}><i className="fab fa-github-alt align-middle"></i></a>
+            <a href="https://drive.google.com/file/d/1Yc14-pB_1-PVFkxMnM71S74sO6Z2bGUr/view?usp=sharing">Resume</a>
+            <a href="mailto:as174@rice.edu">Email</a>
+            <a href="https://www.linkedin.com/in/adam-schueller/">Linkedin</a>
+            <a href="https://github.com/theshoe1029">Github</a>
           </div>
         </footer>
 
